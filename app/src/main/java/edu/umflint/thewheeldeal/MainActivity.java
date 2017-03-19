319d444c4f6eb7c24a81c0503327cb6c0239cdd5
Nicholas Sammut <njsammut95@gmail.com>
2017-03-19 13:35:00 -0400

    UI Changes

--- app/src/main/java/edu/umflint/thewheeldeal/MainActivity.java
+++ app/src/main/java/edu/umflint/thewheeldeal/MainActivity.java
@@ -1,13 +1,47 @@
 package edu.umflint.thewheeldeal;
 
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GestureDetectorCompat gestureObject;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gestureObject = new GestureDetectorCompat(this, new LearnGesture());
+
+    }
+
+    @Override
+        public boolean onTouchEvent(MotionEvent event) {
+        this.gestureObject.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class LearnGesture extends GestureDetector.SimpleOnGestureListener{
+        @Override
+        public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY){
+            if (event2.getX() > event1.getX()){
+            //left to right swipe
+                Intent intent = new Intent(
+                        MainActivity.this, Main2Activity.class);
+                finish();
+                startActivity(intent);
+            }
+            else if (event2.getX() < event1.getX()){
+            //right to left swipe
+            }
+            return true;
+        }
     }
 }
